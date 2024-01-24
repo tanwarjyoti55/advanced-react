7d8e70af201d165f04707a1d56f6642810c98324
tanwarjyoti55 <jyotitanwar20141@gmail.com>
2024-01-24 14:17:50 +0530

    completed the container component

---
+++ src/components/data-source.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import React, { useState,  useEffect } from "react"
+
+export const DataSource = ({getData = ()=>{}, resourceName, children}) =>{
+    const [resource, setResource]= useState(null);
+    useEffect (()=>{
+        (async ()=>{
+            const data= await getData();
+            setResource(data)
+        })();
+    },[getData])
+    return (
+        <>
+        {React.Children.map(children, (child)=>{
+            if(React.isValidElement(child)){
+                return React.cloneElement(child, {[resourceName]: resource})
+            }
+            return child;
+        })}
+        </>
+    )
+}
