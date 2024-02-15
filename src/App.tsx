dcac33fd72ef7b8586d24916230bad78d7c1c2f3
tanwarjyoti55 <jyotitanwar20141@gmail.com>
2024-02-15 13:51:46 +0530

    typing-state (useState)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ShoppingCard from "./components/shopping-card";
+
+function App() {
+  return (
+    <main className="flex items-center h-full place-content-center bg-cyan-900">
+    <ShoppingCard />
+  </main>
+  );
+}
+
+export default App;
