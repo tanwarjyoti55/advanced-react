bfcf1dcf480795ace0437062ef20d590a1b880e9
tanwarjyoti55 <jyotitanwar20141@gmail.com>
2024-02-16 13:48:19 +0530

    typing use Ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,26 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Loader from "./components/loader/loader";
-import Book from "./components/books/book";
-import Books from "./components/books/books";
-import "./App.css";
+import { useRef, useState } from "react";
 
-export type Book = {
-  id: number;
-  title: string;
-  author?: string;
-};
-
-export const fetchRandomBook = async () => {
-  const response = await axios.get(`http://localhost:5000/api/books/random`);
-  return response.data;
-};
-
-export const fetchBook = async (count: number) => {
-  const response = await axios.get(
-    `http://localhost:5000/api/books?limit=${count}`
-  );
-  return response.data;
-};
+export const useUrl = (defaultUrl: string)=>{
+  const [url, setUrl] = useState(defaultUrl)
+  return [url, setUrl] as const;
+}
 
 function App() {
-  // const [book, setBook] = useState<Book | undefined>();
-  const [books, setBooks] = useState<Book[]>([]);
-  const [count, setCount] = useState(10);
+//  const inputRef= useRef<HTMLInputElement>(null);
+//  const mutableRefValue = useRef<number | undefined>();
 
-  // useEffect(() => {
-  //   fetchRandomBook().then(setBook);
-  // }, []);
+//  mutableRefValue.current=20;
 
-  // if (!book) return <Loader />;
-  if (!books) return <Loader />;
+//  return(
+//   <>
+//   <input ref={inputRef}/>
+//   </>
+//  );
 
-  return (
-    <main className="w-full max-w-2xl py-16 mx-auto">
-      {/* <Book title={book.title} author={book.author} /> */}
-      <Books
-        count={count}
-        onChange={(e) => setCount(e.target.valueAsNumber)}
-        onSubmit={() => fetchBook(count).then(setBooks)}
-      >
-        {books.map((book) => {
-          return <Book title={book.title} author={book.author} />;
-        })}
-      </Books>
-    </main>
-  );
+const [url, setUrl] = useUrl('google.com');
+
+return <></>
+
 }
 
 export default App;
